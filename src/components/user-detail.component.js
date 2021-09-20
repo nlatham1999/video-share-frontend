34df067d447771cb7ccf988e9da5863a1a555f61
Nick Latham <nlatham@zagmail.gonzaga.edu>
2021-09-19 19:42:46 -0700

    feat: scaffolding, added todos

--- src/components/user-detail.component.js
+++ src/components/user-detail.component.js
@@ -12,8 +12,34 @@ const UserDetail = ({user}) => {
     return (
         <div>
             {user.email}
+
+            {/* TODO: list out all media in rows 
+                    - Need buttons for deleting and viewing accessors
+                    - need download button*/}
+            {/* TODO: Popup for viewing all accessors to a media file
+                    - can add a new one
+                    - can delete a current one */}
+            {/* TODO: Upload button */}
+            {/* TODO: Upload popup 
+                    - select file, make sure it's mp4 and upload */}
         </div>
     )
+
+    function getMediaForUser(){
+
+    }
+
+    function deleteSingleMedia(){
+
+    }
+
+    function updateOwnersForMedia(){
+
+    }
+
+    function addMedia(){
+
+    }
 
 }
 
